7884477d167177072c528a2ed00bef0fe419ded9
AlexanderLako <100615815+AlexanderLako@users.noreply.github.com>
2022-11-30 22:08:34 -0500

    update connect

--- project_3005.py
+++ project_3005.py
@@ -6,39 +6,48 @@ import psycopg2
 
 # user code
 
+SQLusername = "postgres"
+SQLpassword = "Alextobor2002"
+
+SQLstring = "dbname=3005Project user={} password={}".format(SQLusername, SQLpassword)
+
+
+conn = None
+conn = psycopg2.connect(SQLstring)
+cur = conn.cursor()
+
 def connect():
 
-
+    print("Connection in progress...")
 
     """ Connect to the PostgreSQL database server """
-    conn = None
-    try:
-        # read connection parameters
-        params = config()
+    #conn = None --------------------------------------------------------------------------------------
+    # read connection parameters
 
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect("dbname=3005Project user=postgres password=postgres")
-		
-        # create a cursor
-        cur = conn.cursor()
-        
-	# execute a statement
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
+    # connect to the PostgreSQL server
+    print('Connecting to the PostgreSQL database...')
+    
+    
+    # create a cursor
+    
+    
+# execute a statement
+    print('PostgreSQL database version:')
+    cur.execute('SELECT version()')
 
-        # display the PostgreSQL database server version
-        db_version = cur.fetchone()
-        print(db_version)
+    # display the PostgreSQL database server version
+    db_version = cur.fetchone()
+    print(db_version)
+    disconnect()
        
-	# close the communication with the PostgreSQL
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Database connection closed.')
+	
+
+def disconnect():
+    # close the communication with the PostgreSQL
+    cur.close()
+    if conn is not None:
+        conn.close()
+        print('Database connection closed.')
 
 
 # ask user to login or register, then brings up cart
