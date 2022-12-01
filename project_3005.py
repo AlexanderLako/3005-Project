1b89d6a6c2232b4d99a7bfdb1f423b2933f1515f
AlexanderLako <100615815+AlexanderLako@users.noreply.github.com>
2022-11-30 21:21:38 -0500

    Database connection

--- project_3005.py
+++ project_3005.py
@@ -1,9 +1,44 @@
 # remember in prompts, add the character number restrictions
 # ex. billing info needs to be exactly 9 characters etc.
 
-
+import psycopg2
+#from config import config
 
 # user code
+
+def connect():
+
+
+
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # read connection parameters
+        params = config()
+
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect("dbname=3005Project user=postgres password=postgres")
+		
+        # create a cursor
+        cur = conn.cursor()
+        
+	# execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+       
+	# close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
 
 
 # ask user to login or register, then brings up cart
@@ -327,6 +362,9 @@ def remove_book():
 
 # main loop
 def main():
+
+    connect()
+
     while (True):
         print("\nEnter 1 if you are a user")
         print("Enter 2 if you are an owner")
